c01e89330e6164a3a83c17880fb5f6d0132b9fd4
Leon Pearce <leonp1991@gmail.com>
2017-08-13 17:07:46 +1000

    ADD: no-block-arrow-callback's definition

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   rules: {
     'enforce-i18n-keys': require('./lib/rules/enforce-i18n-keys'),
+    'no-block-arrow-callbacks': require('./lib/rules/no-block-arrow-callbacks'),
     'no-disallowed-props': require('./lib/rules/no-disallowed-props'),
   }
 }

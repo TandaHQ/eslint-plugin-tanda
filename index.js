48856dd0b036685895e7c99a3613b018c6f1f4bf
Dave Allie <dave@tanda.co>
2018-07-30 18:31:51 +1000

    Export expect-expect rule

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   rules: {
     'enforce-i18n-keys': require('./lib/rules/enforce-i18n-keys'),
+    'expect-expect': require('./lib/rules/expect-expect'),
     'no-disallowed-props': require('./lib/rules/no-disallowed-props'),
     'no-equals': require('./lib/rules/no-equal'),
   },
@@ -8,6 +9,8 @@ module.exports = {
     recommended: {
       /* text fields in react components must not be strings */
       "tanda/enforce-i18n-keys": 2,
+      /* jest tests should call expect at least once */
+      "tanda/expect-expect": 2,
       /* don't allow props like className and colour */
       "tanda/no-disallowed-props": 2,
     },

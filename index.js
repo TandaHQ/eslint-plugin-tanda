ce7e1ac0b9da4a24f794102f0fb58b6651669422
Alex Ghiculescu <alex@tanda.co>
2019-08-16 15:44:57 -0500

    omg you have to export the rule

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
     'expect-expect': require('./lib/rules/expect-expect'),
     'no-disallowed-props': require('./lib/rules/no-disallowed-props'),
     'no-equals': require('./lib/rules/no-equal'),
+    'formattedtime-requires-timezone': require('./lib/rules/formattedtime-requires-timezone'),
   },
   configs: {
     recommended: {
@@ -13,6 +14,8 @@ module.exports = {
       "tanda/expect-expect": 2,
       /* don't allow props like className and colour */
       "tanda/no-disallowed-props": 2,
+      /* if you're using <FormattedTime> from react-intl you must provide a time zone */
+      "tanda/formattedtime-requires-timezone": 2,
     },
   },
 }

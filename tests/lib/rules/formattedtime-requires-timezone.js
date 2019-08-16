adcc98504a43c5e3a19295fac0d76fcc380710a5
Alex Ghiculescu <alex@tanda.co>
2019-08-16 13:58:24 -0500

    New rule: formattedtime-requires-timezone

---
+++ tests/lib/rules/formattedtime-requires-timezone.js
@@ -0,0 +1,39 @@
+const rule = require('../../../lib/rules/formattedtime-requires-timezone');
+const { RuleTester } = require('eslint');
+
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+    jsx: true,
+  },
+};
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions });
+ruleTester.run('formattedtime-requires-timezone', rule, {
+  valid: [
+    {
+      code: '<FormattedTime timezone="my text" />',
+    },
+    {
+      code: '<FormattedTime timezone="my text" otherProp={true} />',
+    },
+    {
+      code: '<FormattedTime otherProp={true} timezone="my text" />',
+    },
+  ],
+
+  invalid: [
+    {
+      code: '<FormattedTime foo="bar" />',
+      errors: [{
+        message: 'You must provide a `timezone` to the `FormattedTime` component',
+      }],
+    },
+  ],
+});
